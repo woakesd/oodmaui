c80eb7d2ab7337232cb66a9405f44acaabfb2a40
David Woakes <david@woak.es>
2024-10-13 14:48:20 +0100

    First commit of code

---
+++ OodMaui/ViewModels/AboutViewModel.cs
@@ -0,0 +1,21 @@
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace OodMaui.ViewModels;
+
+internal class AboutViewModel
+{
+    public string Title = AppInfo.Name;
+    public string Version = AppInfo.Version.ToString();
+    public string MoreInfoUrl = "https://aka.ms/maui";
+    public string Message = "This app is written in Xaml and C# with .NET MAUI.";
+    public ICommand ShowMoreInfoCommand { get; }
+
+    public AboutViewModel()
+    {
+        ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfoAsync);
+    }
+
+    async Task ShowMoreInfoAsync() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrl);
+}
---
+++ OodMaui/Views/AboutPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls;
+
+namespace OodMaui.Views;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+}
